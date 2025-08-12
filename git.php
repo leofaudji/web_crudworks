24dea5b2ec17ab9ff9a460ec365005cea4c830b5
leofaudji <leofaudji@gmail.com>
2025-08-12 13:56:20 +0700

    edit folder

--- git.php
+++ git.php
@@ -12,7 +12,7 @@ $hash = hash_hmac('sha1', $request_body, $secret);
 
 if (hash_equals($hash, $hub_signature_parts[1])) {
     // Jalankan git pull
-    chdir('/home/crudwork/website');
+    chdir('/home/crudwork/website3');
     $output = shell_exec('git pull origin main 2>&1');
     echo "<pre>$output</pre>";
     // Log aktivitas (opsional)
